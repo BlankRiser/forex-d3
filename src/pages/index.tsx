2a951a7f4f1e32927ea1435444cc16faef79307f
BlankRiser <rschoudhary1999@gmail.com>
2023-01-30 18:00:24 +0530

    symbol data for candle chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import { Button } from '@/component';
 import { API_URL, BASE_URL } from '@/constants';
@@ -14,6 +14,7 @@ export default function Home() {
   const [symbolQuery, setSymbolQuery] = useState<string>('');
   const debouncedValue = useDebounce<string>(symbolQuery, 500);
   const [symbolResult, setSymbolResult] = useState([]);
+  const [selectedSymbol, setSelectedSymbol] = useState<string>('');
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSymbolQuery(event.target.value);
@@ -40,66 +41,65 @@ export default function Home() {
     return data;
   }
 
-  async function getStockCandle(options: RequestInit) {
+  async function getStockCandle(options?: RequestInit) {
     const data = await getFinnhubData({
       url:
         BASE_URL.FINHUB_BASE_URL +
         API_URL.FINHUB.STOCK_CANDLES +
-        '?symbol=AAPL&resolution=D&from=1590988249&to=1591852249',
+        `?symbol=${selectedSymbol}&resolution=D&from=1590988249&to=1591852249`,
       options: options,
     });
     console.log(data);
     return data;
   }
 
-  // useEffect(() => {
-  //   if (symbolQuery.length > 0) {
-  //     getStockCandle();
-  //   }
+  useEffect(() => {
+    if (selectedSymbol.length > 0) {
+      getStockCandle();
+    }
 
-  //   return () => {};
-  // }, [symbolQuery]);
+    return () => {};
+  }, [selectedSymbol]);
 
-  // if (status === 'loading') {
-  //   return <p>Hang on there...</p>;
-  // }
+  if (status === 'loading') {
+    return <p>Hang on there...</p>;
+  }
 
-  // if (status === 'authenticated') {
-  //   return (
-  //     <div className='grid h-screen w-screen place-items-center'>
-  //       <Button onClick={() => signOut()}>Sign out: as {userEmail}</Button>
-  //     </div>
-  //   );
-  // }
+  if (status === 'authenticated') {
+    return (
+      <div className='grid h-screen w-screen place-items-center'>
+        <Button onClick={() => signOut()}>Sign out: as {userEmail}</Button>
+        <input
+          type='text'
+          value={symbolQuery}
+          onChange={handleChange}
+          className='border bg-gray-50 text-gray-800 focus:outline-none'
+        />
+
+        {symbolResult.length > 0 ? (
+          <select
+            name=''
+            id=''
+            onChange={(e) => {
+              setSelectedSymbol(e.target.value);
+            }}
+          >
+            {symbolResult?.slice(0, 10)?.map((item: any) => (
+              <option key={item.symbol} value={item.symbol}>
+                {item.symbol}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <div className='animate-pulse rounded-sm bg-gray-200 px-4 py-2'>Loading</div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className='grid h-screen w-screen place-items-center'>
       <Button onClick={() => signIn()}>Sign in</Button>
-
-      <input
-        type='text'
-        value={symbolQuery}
-        onChange={handleChange}
-        className='border bg-gray-50 text-gray-800 focus:outline-none'
-      />
-
-      {symbolResult.length > 0 ? (
-        <select
-          name=''
-          id=''
-          // onChange={(e) => {
-          //   setSymbol(e.target.value);
-          // }}
-        >
-          {symbolResult?.slice(0, 10)?.map((item: any) => (
-            <option key={item.symbol} value={item.symbol}>
-              {item.symbol}
-            </option>
-          ))}
-        </select>
-      ) : (
-        <div className='animate-pulse rounded-sm bg-gray-200 px-4 py-2'>Loading</div>
-      )}
     </div>
   );
 }
